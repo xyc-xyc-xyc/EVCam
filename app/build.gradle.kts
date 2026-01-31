d160dd1a36e0c09bcb738e8dcf23e5cf0dad9253
suyunkai <cn.syk@foxmail.com>
2026-01-31 14:57:15 +0800

    Release v0.9.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     namespace = "com.kooo.evcam"
     compileSdk = 36
 
-    // 缁涙儳鎮曢柊宥囩枂 (娴ｈ法鏁?AOSP 閸忣剙鍙″ù瀣槸缁涙儳鎮?
+    // 缂佹稒鍎抽幃鏇㈡煀瀹ュ洨鏋?(濞达綀娉曢弫?AOSP 闁稿浚鍓欓崣鈥趁圭€ｎ厾妲哥紒娑欏劤閹?
     signingConfigs {
         create("release") {
             storeFile = file("../keystore/release.jks")
@@ -28,7 +28,7 @@ android {
 
     buildTypes {
         release {
-            // 娴ｈ法鏁ょ粵鎯ф倳闁板秶鐤?            signingConfig = signingConfigs.getByName("release")
+            // 濞达綀娉曢弫銈囩驳閹勫€抽梺鏉跨Ф閻?            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -51,22 +51,23 @@ dependencies {
     implementation(libs.recyclerview)
     implementation(libs.cardview)
 
-    // 闁藉鎷ょ€规ɑ鏌?Stream SDK
+    // 闂佽棄顦甸幏銈団偓瑙勆戦弻?Stream SDK
     implementation("com.dingtalk.open:app-stream-client:1.3.12")
 
-    // 椋炰功锛氫娇鐢ㄨ交閲忕骇 OkHttp WebSocket 瀹炵幇锛屼笉鍐嶄緷璧栧畼鏂?SDK
+    // 妞嬬偘鍔熼敍姘▏閻劏浜ら柌蹇曢獓 OkHttp WebSocket 鐎圭偟骞囬敍灞肩瑝閸愬秳绶风挧鏍х暭閺?SDK
 
-    // 缃戠粶璇锋眰鍜?WebSocket
+    // 缂冩垹绮剁拠閿嬬湴閸?WebSocket
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    // JSON 鐟欙絾鐎?    implementation("com.google.code.gson:gson:2.10.1")
+    // JSON 瑙ｆ瀽
+    implementation("com.google.code.gson:gson:2.10.1")
 
-    // Glide 閸ュ墽澧栭崝鐘烘祰鎼存搫绱欓悽銊ょ艾缂傛挸鐡ㄩ崪灞肩喘閸栨牜缂夐悾銉ユ禈閸旂姾娴囬敍?
+    // Glide 闁搞儱澧芥晶鏍礉閻樼儤绁伴幖瀛樻惈缁辨瑩鎮介妸銈囪壘缂傚倹鎸搁悺銊╁椽鐏炶偐鍠橀柛鏍ㄧ墱缂傚鎮鹃妷銉︾闁告梻濮惧ù鍥晬?
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
-    // WorkManager 鐎规碍妞傛禒璇插閿涘牏鏁ゆ禍搴濈箽濞蹭紮绱?    implementation("androidx.work:work-runtime:2.9.0")
+    // WorkManager 閻庤纰嶅鍌涚鐠囨彃顫ら柨娑樼墢閺併倖绂嶆惔婵堢婵炶弓绱槐?    implementation("androidx.work:work-runtime:2.9.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
